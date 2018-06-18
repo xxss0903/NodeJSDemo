97902fc4c5655be0e014eefb358e3e16a8521432
xxss0903 <353768356@qq.com>
2018-06-19 01:28:25 +0800

    'update'

--- server.js
+++ server.js
@@ -4,72 +4,134 @@ let express = require('express');
 let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
-app.use(express.static(__dirname + '/public'));
 
-server.listen(9999, function () {
-    console.log('listen 9999');
+
+server.listen(5757, function () {
+    console.log('listen 5757');
 })
+
+// 大厅
+let hall = null;
+// 匹配队列
+let queue = null;
+// 最大房间数
+let MAX = 30;
+// 游戏房间
+let rooms = [];
+
+function Room() {
+    this.people = 0;
+    this.socket = null;
+}
+
+function Queue() {
+    this.people = 0;
+    this.socket = null;
+}
+
+function Hall() {
+    this.people = 0;
+    this.socket = null;
+}
+
+hall = new Hall();
+queue = new Queue();
+
+// 创造这么多个房间
+for (let n = 0; n < MAX; n++) {
+    rooms[n] = new Room();
+}
+
+function getFreeRoom(){
+    for(let n = 0; n < MAX; n++){
+        if(rooms[n].people === 0){
+            return n;
+        }
+    }
+    return -1;
+}
+
+io.people = 0;
 
 let room = "room1";
 
 io.on('connection', function (socket) {
-    console.log('someone connected 是谁 ' + socket.id + " #" + socket.msg);
-    io.emit('connected', 'You connected me');
-    // 接收到微信的消息，然后发送play消息出去
-    try {
-        // 添加到相同得房间
-        socket.join(room, function () {
-            console.log(socket.id + ' 加入了 房间 ' + socket.rooms);
-        })
+    io.people++;
+    console.log('someone connected 是谁 ' + socket.id + " #" + io.people);
 
-        socket.on('disconnect', function (data) {
-            console.log('断开链接');
-            io.emit('off', '离开')
-        })
-
-        socket.on('football', function (msg) {
-            console.log('打球 ' + msg);
-            io.emit('football', '打球呀傻子' + msg);
-        })
-
-        socket.on('error', function (msg) {
-            console.log('链接错误 ' + msg);
-        })
-
-        socket.on('disconnect', (reason) => {
-            console.log('断开链接 ' + reason);
-        })
-
-        // 接收到手柄的消息
-        socket.on('handplay', function (msg) {
-            // io.emit('handplay', msg);
-            var myroom = Object.keys(socket.rooms)[1];
-            var obj = JSON.parse(msg);
-            obj.room = myroom;
-            msg = JSON.stringify(obj);
-            console.log(msg);
-            io.to(myroom).emit('handplay', msg);
-        })
-
-        // 接受到游戏界面的消息，选择游戏模式
-        socket.on('gamemode', function (msg) {
-            var mode = {};
-            var myroom = Object.keys(socket.rooms)[1];
-            mode.mode = msg;
-            mode.room = myroom;
-            var modeStr = JSON.stringify(mode);
-            console.log(' 游戏模式 ' + modeStr);
-            io.to(myroom).emit('selectmode', modeStr);
-        });
-
-        // 监听射球的状态，完成射击，开始踢球等
-        socket.on('shootstatus', function (msg) {
-            var myroom = Object.keys(socket.rooms)[1];
-            console.log("射球状态 " + msg);
-            io.to(myroom).emit('handstatus', msg);
-        })
-
-    } catch (err) {
-        console.log(err)
-    }
+    socket.on('disconnect', function () {
+        io.people--;
+        console.log('断开链接');
+    })
 })
+
+queue.socket = io.of('/queue').on('connection', function(socket){
+    queue.people++;
+    console.log('some one connect queue socket. ' + queue.people);
+    if(queue.people === 1){
+        let roomId = getFreeRoom();
+        console.log('拿到房间 ' + roomId);
+        socket.emit('game mode', 'single');
+        if (roomId >= 0) {
+            queue.socket.emit('match success', roomId);
+            console.log('match success. ' + queue.people + ' in queue');
+        } else {
+            console.log('no free room');
+        }
+    } else if(queue.people === 2){
+        socket.emit('game mode', 'multi');
+        let roomId = getFreeRoom();
+        console.log('拿到房间 ' + roomId);
+        if(roomId >= 0){
+            queue.socket.emit('match success', roomId);
+            console.log('match success. ' + queue.people + ' in queue');
+        } else {
+            console.log('no free room');
+        }
+    }
+
+    socket.on('cancel match', function(){
+        queue.people--;
+        console.log('some cancel match ' + queue.people);
+    })
+
+    socket.on('disconnect', function(){
+        queue.people--;
+        console.log('some disconneced match ' + queue.people);
+    })
+})
+
+hall.socket = io.of('/hall').on('connection', function (socket) {
+    hall.people++;
+    console.log('a player connected. ' + hall.people);
+    hall.socket.emit('people changed', hall.people);
+
+    socket.on('disconnect', function () {
+        hall.people--;
+        console.log('a player disconnected. left ' + hall.people + ' in hall');
+        hall.socket.emit('people changed', hall.people);
+    })
+})
+
+// 同时创建这么多个房间的链接的socket监听
+for (let i = 0; i < MAX; i++) {
+    rooms[i].socket = io.of('/rooms' + i).on('connection', function (socket) {
+        rooms[i].people++;
+        console.log('房间' + i + " 有 " + rooms[i].people+'个人');
+        // 射球开始
+        socket.on('shootstart', function (info) {
+            socket.broadcast.emit('shootstart', info);
+        })
+
+        // 射球结束，如果是多人，则切换当前射门的选手
+        socket.on('shootend', function () {
+            socket.broadcast.emit('shootend');
+        })
+
+        socket.on('disconnect', function () {
+            rooms[i].people--;
+            console.log('someone disconnected room ' + i);
+        })
+
+    })
+}
